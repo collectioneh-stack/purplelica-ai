2f9ea395b3b7b57748a7383b7f5a158eb180d411
collectioneh-stack <collectioneh@gmail.com>
2026-05-19 22:17:59 +0900

    feat: Purplelica AI 블로그 초기 세팅

---
+++ data/headerNavLinks.ts
@@ -0,0 +1,9 @@
+const headerNavLinks = [
+  { href: '/', title: '홈' },
+  { href: '/blog', title: '블로그' },
+  { href: '/tags', title: '태그' },
+  { href: '/projects', title: '프로젝트' },
+  { href: '/about', title: '소개' },
+]
+
+export default headerNavLinks
